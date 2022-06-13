6fe41b7ffc4485c62ff1d08878a8e047ca7ff9a0
linetlucy-genchabe <linetlucy21@gmail.com>
2022-06-13 22:41:58 +0300

    ft:added functions

--- lynneawwards/views.py
+++ lynneawwards/views.py
@@ -1,3 +1,156 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login,logout
+from .models import Profile
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ProfileSerializer, ProjectSerializer
+from .forms import *
+
+
+# Create your views here.
+
+
+
+def home(request):
+    return render (request, 'index.html')
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"] 
+        
+        user = authenticate (request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"You are successfuly logged in")
+            return redirect ("/")
+    return render (request, 'registration/login.html')
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        first_name=request.POST["first_name"]
+        last_name=request.POST["last_name"]
+        email=request.POST["email"]
+        password1=request.POST["password1"]
+        password2=request.POST["password2"]
+
+        if password1 != password2:
+            messages.error(request,"Passwords do not match")
+            return redirect('/register')
+
+        new_user = User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password1
+
+        )
+        new_user.save()
+
+        new_profile = Profile.objects.create(
+            user=new_user,
+            bio="",
+            profile_picture="",
+        )
+        new_profile.save()
+        return render(request,'registration/login.html')
+
+    return render (request, 'registration/registration.html')
+
+
+def signout(request):
+    logout(request)
+    messages.success(request,"You have logged out")
+           
+    return redirect("/")
+
+@login_required
+def profile(request):
+    user = request.user
+    my_profile = Profile.objects.get(user=user)
+    print(my_profile)
+    return render(request,"profile.html",{'my_profile':my_profile,'user':user})
+
+    
+@login_required(login_url='/accounts/login/')
+def search_projects(request):
+    if 'keyword' in request.GET and request.GET["keyword"]:
+        search_term = request.GET.get("keyword")
+        searched_projects = Projects.search_projects(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {"message":message,"projects": searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message": message})
+
+
+def get_project(request, id):
+
+    try:
+        project = Projects.objects.get(pk = id)
+        
+    except ObjectDoesNotExist:
+        raise Http404()
+    
+    
+    return render(request, "projects.html", {"project":project})
+  
+
+@login_required(login_url='/accounts/login/')
+def new_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.Author = current_user
+            project.save()
+        return redirect('index')
+
+    else:
+        form = NewProjectForm()
+    return render(request, 'new-project.html', {"form": form})
+
+
+@login_required(login_url='/accounts/login/')
+def user_profiles(request):
+    current_user = request.user
+    Author = current_user
+    projects = Projects.get_by_author(Author)
+    
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.save()
+        return redirect('profile')
+        
+    else:
+        form = ProfileUpdateForm()
+    
+    return render(request, 'registration/profile.html', {"form":form, "projects":projects})
+
+
+class ProjectList(APIView):
+    def get(self, request, format=None):
+        all_project = Projects.objects.all()
+        serializers = ProjectSerializer(all_project, many=True)
+        return Response(serializers.data)
+    
+    
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        all_profile = Profile.objects.all()
+        serializers = ProfileSerializer(all_profile, many=True)
+        return Response(serializers.data)
+    
