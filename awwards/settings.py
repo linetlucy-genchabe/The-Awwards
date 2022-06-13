d8c4e3d1c38d69966eb069d5e842444b1964b765
linetlucy-genchabe <linetlucy21@gmail.com>
2022-06-13 22:27:42 +0300

    added email settings

--- awwards/settings.py
+++ awwards/settings.py
@@ -36,11 +36,14 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = []
 
 # Email configurations remember to install python-decouple
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+
+EMAIL_USE_TLS=True
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='linetlucy21@gmail.com'
+EMAIL_HOST_PASSWORD='Linet.2211'
+
 
 
 # Application definition
